4eedb12a1b3cb9e718792476d30b5107b8f83686
Andrea <andrea3aprile99@gmail.com>
2021-04-09 16:20:36 +0200

    f

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var app = new Vue(
 	el: '#root',
 	data: {
 		emailList: [],
+		generes:[]
 	},
 	methods: {
 
@@ -12,8 +13,15 @@ var app = new Vue(
 
 			axios.get('https://flynn.boolean.careers/exercises/api/array/music')
 			.then( (risposta)=>{
-				this.emailList.push(risposta.data.response);
+				this.emailList=(risposta.data.response);
 				// console.log(this.emailList);
+				this.emailList.forEach((item, i) => {
+					if (this.generes.includes(item.generes) == false) {
+	item.generes.push(item.generes);
+					}
+
+				});
+
 			});
 
 	},
